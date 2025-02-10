23d97b06b16f09ebd60932f340b30cb3c9d1074b
sorokovovaa <sorokovovaa@1X06X14>
2025-02-10 18:30:47 +0400

    всё сделал + комменты

--- Task/RecursionBinarySearch.cpp
+++ Task/RecursionBinarySearch.cpp
@@ -5,45 +5,31 @@
 #include "Task.h"
 #include "algorithm"
 
-void NewArray(int* array, int middle)
+int RecursionTarget(int* array, int left, int right, int target) 
 {
-	//int* newArray = new int[middle];
-	for (int i = 0; i < middle; i++)
-		array[i] = array[i];
-	//return array;
-}
+	int middle = left + (right - left) / 2;							// поиск середины массива
 
-void NewArray2(int* array, int middle, int size)
-{
-	//int* newArray = new int[middle];
-	for (int i = size-1, j = 0; i > middle; i--, j++)
-		array[j] = array[i];
-	//return array;
+	if (array[middle] == target) 
+	{
+		while (array[middle] == target)								// поиск элемента, с которого начинаются повторяющиеся числа
+			middle--;
+		if (array[middle] != target)								// если middle перебрал слишком далеко влево
+			middle++;												// возвращаем его, сдвигая middle вправо
+		return middle;
+	}
+	if (middle > target)											// если искомое значение меньше середины
+		return RecursionTarget(array, left, middle - 1, target);	// сдвигаем правую границу
+	else if (middle < target)										// если искомое значение больше середины
+			return RecursionTarget(array, middle + 1, right, target);	// сдвигаем левую границу
 }
 
 int RecursionBinarySearch(int* array, int size, int target)
 {
-	if (array == nullptr || size < 1) return -1;
-	//int index = 0;
-	int right = size - 1;
-	int middle = right / 2;
-	if (target > array[middle])
-	{
-		NewArray2(array, middle,size);
-		size /= 2;
-	}
-	else if (target <= array[middle])
-	{
-		NewArray(array, middle);
-		size /= 2;
-	}
-	if (size >= 1)
-	{
-
-		RecursionBinarySearch(array, size, target);
-	}
-	if (array[right] == target) return right;
-	else return -1;
+	if (size <= 0 || size == 1 && array[0] != target || target < array[0] || target > size) return -1;	// перебор условий при которых возникают ошибки
+	if (array[0] == target)
+		return 0;													// если первый элемент - искомое значение
+	if (size > 1)
+		return RecursionTarget(array, 0, size - 1, target);			// вызов рекурсивной функции
 }
 
 /*
